0f4fa7f21a113d5a32e66df4829154317ac72a88
dominictarro <dominic.tarro@gmail.com>
2023-04-25 00:33:47 -0400

    test: tests for oryx
    Unit tests for the Oryx ETL pipeline. Includes individual tasks, test configuration, test fixtures, and an end-to-end flow.

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+"""
+Pytest configuration.
+"""
+from pathlib import Path
+
+import pytest
+from prefect.testing.utilities import prefect_test_harness
+
+
+TESTS_PATH: Path = Path(__file__).parent
+
+
+@pytest.fixture
+def output_path() -> Path:
+    """Path to the output directory."""
+    output_path = TESTS_PATH / "output"
+    output_path.mkdir(exist_ok=True)
+    return output_path
+
+
+@pytest.fixture
+def test_data_path() -> Path:
+    """Path to the test data directory."""
+    return TESTS_PATH / "data"
+
+# Sets the test harness so prefect flow tests are not recorded in the Orion
+# database.
+#
+# I use this at the session-level so that all flows are run within the same
+# environment.
+@pytest.fixture(autouse=True, scope="session")
+def with_test_harness():
+    """Sets the Prefect test harness for local pipeline testing."""
+    with prefect_test_harness():
+        yield
+
+
+# pytest_plugins = [
+#     "tests.oryx.conftest",
+# ]
---
+++ tests/oryx/test_parser.py
@@ -0,0 +1,69 @@
+"""
+Tests for the article parser.
+"""
+from borderlands.oryx.oryx_parser.article import ArticleParser
+
+
+def test_ukraine_page_parse(ukraine_article_parser: ArticleParser):
+    """Tests parsing the Ukraine page."""
+    assert ukraine_article_parser
+    result = list(ukraine_article_parser.parse())
+    assert isinstance(result, list)
+    assert all(isinstance(item, dict) for item in result)
+    
+
+def test_russia_page_parse(russia_article_parser: ArticleParser):
+    """Tests parsing the Russia page."""
+    assert russia_article_parser
+    result = list(russia_article_parser.parse())
+    assert isinstance(result, list)
+    assert all(isinstance(item, dict) for item in result)
+
+
+
+def test_ukraine_page_parse_result(ukraine_page_parse_result: list):
+    """Tests the result of parsing the Ukraine page."""
+    assert isinstance(ukraine_page_parse_result, list)
+    assert all(isinstance(item, dict) for item in ukraine_page_parse_result)
+    
+    EXPECTED_KEYS = {
+        "evidence_url",
+        "description",
+        "id_",
+        "model",
+        "country_of_production_flag_url",
+        "category",
+    }
+    assert all(EXPECTED_KEYS.issubset(item.keys()) for item in ukraine_page_parse_result)
+
+    # Test types
+    assert all(isinstance(item["evidence_url"], str) for item in ukraine_page_parse_result)
+    assert all(isinstance(item["description"], str) for item in ukraine_page_parse_result)
+    assert all(isinstance(item["id_"], str) and item["id_"].isnumeric() for item in ukraine_page_parse_result)
+    assert all(isinstance(item["model"], str) for item in ukraine_page_parse_result)
+    assert all(isinstance(item["country_of_production_flag_url"], str) for item in ukraine_page_parse_result)
+    assert all(isinstance(item["category"], str) for item in ukraine_page_parse_result)
+
+
+def test_russia_page_parse_result(russia_page_parse_result: list):
+    """Tests the result of parsing the Russia page."""
+    assert isinstance(russia_page_parse_result, list)
+    assert all(isinstance(item, dict) for item in russia_page_parse_result)
+    
+    EXPECTED_KEYS = {
+        "evidence_url",
+        "description",
+        "id_",
+        "model",
+        "country_of_production_flag_url",
+        "category",
+    }
+    assert all(EXPECTED_KEYS.issubset(item.keys()) for item in russia_page_parse_result)
+
+    # Test types
+    assert all(isinstance(item["evidence_url"], str) for item in russia_page_parse_result)
+    assert all(isinstance(item["description"], str) for item in russia_page_parse_result)
+    assert all(isinstance(item["id_"], str) and item["id_"].isnumeric() for item in russia_page_parse_result)
+    assert all(isinstance(item["model"], str) for item in russia_page_parse_result)
+    assert all(isinstance(item["country_of_production_flag_url"], str) for item in russia_page_parse_result)
+    assert all(isinstance(item["category"], str) for item in russia_page_parse_result)
