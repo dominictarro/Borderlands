c9150954b0b0f1bcd7c2b7e88ab0fac7514489f9
Dominic Tarro <dominic.tarro@gmail.com>
2023-05-02 22:29:45 -0400

    fix(infrastructure): fix ecs execution failure
    Simplified the infrastructure while fixing a bug preventing the ECS task from starting.

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -6,96 +6,77 @@ The ECS cluster the Prefect Agent runs on requires a VPC and subnet to run it in
 subnet must have inbound and outbound routes to the internet.
 */
 
-
-resource "aws_vpc_ipam" "borderlands_ipam" {
-  operating_regions {
-    region_name = data.aws_region.current.name
-  }
+/*
+Network variables.
+*/
+variable "vpc_availability_zone" {
+    type = string
+    description = "Availability zone for the VPC."
+    default = "us-east-1a"
 }
 
-resource "aws_vpc_ipam_pool" "borderlands_ipam_pool" {
-  address_family = "ipv4"
-  ipam_scope_id  = aws_vpc_ipam.borderlands_ipam.private_default_scope_id
-  locale         = data.aws_region.current.name
+variable "vpc_cidr_block" {
+  type = string
+  description = "CIDR for the VPC."
+  default = "10.0.0.0/16"
 }
 
-resource "aws_vpc_ipam_pool_cidr" "boderlands_ipam_pool_cidr" {
-  ipam_pool_id = aws_vpc_ipam_pool.borderlands_ipam_pool.id
-  cidr         = "172.2.0.0/16"
+variable "vpc_public_subnet_cidr_block" {
+  type = string
+  description = "CIDR for the public subnet."
+  default = "10.0.1.0/24"
 }
 
+variable "vpc_private_subnet_cidr_block" {
+  type = string
+  description = "CIDR for the private subnet."
+  default = "10.0.2.0/24"
+}
+
+/*
+Network for Prefect to operate within.
+
+Contains 1 public subnet and 1 private subnet.
+
+To expand the number of subnets, note the changes from this guide
+
+https://spacelift.io/blog/terraform-aws-vpc
+
+
+*/
 resource "aws_vpc" "borderlands_vpc" {
-  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.borderlands_ipam_pool.id
-  ipv4_netmask_length = 20
-  // CIDR must be created before network initialized
-  depends_on  = [
-    aws_vpc_ipam_pool_cidr.boderlands_ipam_pool_cidr
-  ]
+    cidr_block = var.vpc_cidr_block
 }
 
-resource "aws_subnet" "borderlands_subnet" {
-  vpc_id            = aws_vpc.borderlands_vpc.id
-  # Mask must be size of aws_vpc.borderlands_vpc.ipv4_netmask_length
-  cidr_block        = "172.2.0.0/20"
-  // CIDR must be created before subnet initialized
-  depends_on = [
-    aws_vpc_ipam_pool_cidr.boderlands_ipam_pool_cidr
-  ]
+resource "aws_subnet" "borderlands_public_subnet" {
+    vpc_id = aws_vpc.borderlands_vpc.id
+    cidr_block = var.vpc_public_subnet_cidr_block
+    availability_zone = var.vpc_availability_zone
 }
 
-/*
-Traffic control
-*/
-
-resource "aws_network_acl" "borderlands_acl" {
-  vpc_id = aws_vpc.borderlands_vpc.id
+resource "aws_subnet" "borderlands_private_subnet" {
+    vpc_id = aws_vpc.borderlands_vpc.id
+    cidr_block = var.vpc_private_subnet_cidr_block
+    availability_zone = var.vpc_availability_zone
 }
 
-resource "aws_network_acl_rule" "borderlands_ingress" {
-  network_acl_id = aws_network_acl.borderlands_acl.id
-  rule_number    = 100
-  egress         = false
-  protocol       = "-1"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
+resource "aws_internet_gateway" "borderlands_igw" {
+    vpc_id = aws_vpc.borderlands_vpc.id
 }
 
-resource "aws_network_acl_rule" "borderlands_egress" {
-  network_acl_id = aws_network_acl.borderlands_acl.id
-  rule_number    = 100
-  egress         = true
-  protocol       = "-1"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
+resource "aws_route_table" "borderlands_public_rt" {
+ vpc_id = aws_vpc.borderlands_vpc.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.borderlands_igw.id
+ }
+ 
 }
 
-# Associate the subnet with the ACL
-resource "aws_network_acl_association" "borderlands_subnet_acl" {
-  subnet_id      = aws_subnet.borderlands_subnet.id
-  network_acl_id = aws_network_acl.borderlands_acl.id
-}
-
-/*
-Network routing
-*/
-
-resource "aws_internet_gateway" "borderlands_iag" {
-  vpc_id = aws_vpc.borderlands_vpc.id
-}
-
-resource "aws_route_table" "borderlands_rt" {
-  vpc_id = aws_vpc.borderlands_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.borderlands_iag.id
-  }
-}
-
-# Associate the subnet with the route table
-resource "aws_route_table_association" "borderlands_subnet_rt" {
-  subnet_id       = aws_subnet.borderlands_subnet.id
-  route_table_id  = aws_route_table.borderlands_rt.id
+resource "aws_route_table_association" "borderlands_public_subnet_rt_association" {
+    subnet_id = aws_subnet.borderlands_public_subnet.id
+    route_table_id = aws_route_table.borderlands_public_rt.id
 }
 
 
@@ -123,4 +104,13 @@ resource "aws_security_group_rule" "https_outbound" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
 
+}
+
+/*
+Network outputs.
+*/
+
+output "borderlands_vpc_id" {
+    description = "VPC ID for Borderlands AWS infrastructure."
+    value = aws_vpc.borderlands_vpc.id
 }
