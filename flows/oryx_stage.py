e942728466d03b6637e9f7f445f600c38e9369d3
Dominic Tarro <dominic.tarro@gmail.com>
2023-05-05 01:43:30 -0400

    feat: url hash in staged documents
    A sha256 'url_hash' field is calculated for each row.

--- flows/oryx_stage.py
+++ flows/oryx_stage.py
@@ -6,13 +6,16 @@ from __future__ import annotations
 from prefect import flow
 from prefect.context import FlowRunContext, get_run_context
 
-from borderlands.oryx import assets
-from borderlands.oryx import blocks
-from borderlands.oryx.stage.extract import get_russian_equipment_loss_page, get_ukrainian_equipment_loss_page
+from borderlands.oryx import assets, blocks
+from borderlands.oryx.stage.extract import (
+    get_russian_equipment_loss_page,
+    get_ukrainian_equipment_loss_page,
+)
 from borderlands.oryx.stage.transform import (
     assign_country_of_production,
     assign_evidence_source,
     assign_status,
+    calculate_url_hash,
     clean_dataframe,
     convert_to_records,
     flag_duplicate_natural_keys,
@@ -44,17 +47,22 @@ def stage_oryx_equipment_losses() -> list[dict]:
     ukrainian_page = get_ukrainian_equipment_loss_page.submit()
     mapper = assets.get_country_of_production_url_mapper.submit()
 
-    data_ru = parse_equipment_losses_page.submit(russian_page, "Russia", ctx.flow_run.start_time)
-    data_ua = parse_equipment_losses_page.submit(ukrainian_page, "Ukraine", ctx.flow_run.start_time)
+    data_ru = parse_equipment_losses_page.submit(
+        russian_page, "Russia", ctx.flow_run.start_time
+    )
+    data_ua = parse_equipment_losses_page.submit(
+        ukrainian_page, "Ukraine", ctx.flow_run.start_time
+    )
     df_ru = tabulate_loss_cases.submit(data_ru)
     df_ua = tabulate_loss_cases.submit(data_ua)
-    
+
     df = concat([df_ru, df_ua], ignore_index=True)
     df = clean_dataframe(df)
     df = assign_status(df)
     df = assign_country_of_production(df, mapper)
     df = assign_evidence_source(df)
     df = flag_duplicate_natural_keys(df)
+    df = calculate_url_hash(df)
 
     records = convert_to_records(df)
     datestring = ctx.flow_run.start_time.isoformat().replace(":", "-").replace(" ", "_")
