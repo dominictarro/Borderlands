4b0fc3336dfde63042c4dcca4a6a9b0a3d6564ba
Dominic Tarro <dominic.tarro@gmail.com>
2023-06-18 16:59:52 -0400

    new prefect blocks

--- blocks/tf.py
+++ blocks/tf.py
@@ -2,6 +2,14 @@
 Terraform variables.
 """
 
-PREFECT_AGENT_EXECUTION_ROLE_ARN = "arn:aws:iam::604611895840:role/prefect-agent-execution-role-borderlands-prefect"
-PREFECT_AGENT_TASK_ROLE_ARN = "arn:aws:iam::604611895840:role/prefect-agent-task-role-borderlands-prefect"
+PREFECT_AGENT_EXECUTION_ROLE_ARN = (
+    "arn:aws:iam::604611895840:role/prefect-agent-execution-role-borderlands-prefect"
+)
+PREFECT_AGENT_TASK_ROLE_ARN = (
+    "arn:aws:iam::604611895840:role/prefect-agent-task-role-borderlands-prefect"
+)
 PREFECT_AGENT_CLUSTER_NAME = "prefect-agent-borderlands-prefect"
+
+CORE_BUCKET_NAME = "borderlands-core"
+PERSISTENCE_BUCKET_NAME = "borderlands-persistence"
+MEDIA_BUCKET_NAME = "borderlands-media"
