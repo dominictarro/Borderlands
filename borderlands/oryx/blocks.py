77e97e5ade9aaadaa7466f38667c1163b7a55b62
Dominic Tarro <dominic.tarro@gmail.com>
2023-06-18 17:24:00 -0400

    update buckets in flows

--- borderlands/oryx/blocks.py
+++ borderlands/oryx/blocks.py
@@ -1,15 +1,11 @@
 """
 Blocks for the Oryx pipeline.
 """
-from ..storage import bucket
+from ..storage import bucket, media_bucket, persistence_bucket
 from ..utilities.blocks import create_child_bucket
 
-oryx_bucket = create_child_bucket("oryx", "-oryx", parent=bucket, save=True)
-landing_bucket = create_child_bucket(
-    "landing", "-landing", parent=oryx_bucket, save=True
-)
-assets_bucket = create_child_bucket("assets", "-assets", parent=oryx_bucket, save=True)
-persistence_bucket = create_child_bucket(
-    "persistence", "-persistence", parent=oryx_bucket, save=True
-)
-media_bucket = create_child_bucket("media", "-media", parent=oryx_bucket, save=True)
+oryx_bucket = bucket
+landing_bucket = create_child_bucket("landing", "-landing", parent=bucket, save=True)
+assets_bucket = create_child_bucket("assets", "-assets", parent=bucket, save=True)
+persistence_bucket = persistence_bucket
+media_bucket = media_bucket
