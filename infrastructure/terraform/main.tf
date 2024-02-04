e29a4b7c9add6828fbef41205410ae08343261bb
Dominic Tarro <dominic.tarro@gmail.com>
2024-02-04 01:11:25 -0500

    Bumped terraform major version

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,9 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  profile = "default"
 }
 
 // Region speficied in AWS provider
