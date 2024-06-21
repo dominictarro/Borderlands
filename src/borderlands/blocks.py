199151554fa3dec81ee0138237d6f21753ca89c9
Dominic Tarro <57306102+dominictarro@users.noreply.github.com>
2024-06-20 23:21:07 -0400

    Use Prefect artifacts over Slack (#52)
    * Fix count key in alert_on_unmapped_country_flags function
    
    * refactor: Swap unmapped flag URL Slack webhook for Prefect Artifact
    
    Switch unmapped flag URL Slack notification with a Prefect artifact. Remove references to `prefect-slack`.
    
    * Relock Pipfile.lock

--- src/borderlands/blocks.py
+++ src/borderlands/blocks.py
@@ -6,14 +6,12 @@ import asyncio
 
 from prefect.utilities.asyncutils import sync_compatible
 from prefect_aws import S3Bucket
-from prefect_slack import SlackWebhook
 
 
 class Blocks:
     """Class for lazy loading Prefect Blocks."""
 
     _bucket: S3Bucket | None = None
-    _webhook: SlackWebhook | None = None
 
     @property
     def bucket(self) -> S3Bucket:
@@ -22,22 +20,11 @@ class Blocks:
             self._bucket = S3Bucket.load("s3-bucket-borderlands-core")
         return self._bucket
 
-    @property
-    def webhook(self) -> SlackWebhook:
-        """Returns the webhook for the program. Loads if it isn't already."""
-        if not self._webhook:
-            self._webhook = SlackWebhook.load("slack-webhook-borderlands")
-        return self._webhook
-
     @sync_compatible
     async def load(self):
         """Load the blocks."""
         self._bucket = (
             (await self.bucket) if asyncio.iscoroutine(self.bucket) else self.bucket
-        )
-
-        self._webhook = (
-            (await self.webhook) if asyncio.iscoroutine(self.webhook) else self.webhook
         )
 
 
